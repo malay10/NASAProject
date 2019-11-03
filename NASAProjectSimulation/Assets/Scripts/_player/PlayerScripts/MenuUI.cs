1710a217da8267a5394308ca1ce09353686020fb
malay10 <malaybhakta10@gmail.com>
2019-11-02 22:20:45 -0500

    TraineeMovement, and raycast

---
+++ NASAProjectSimulation/Assets/Scripts/_player/PlayerScripts/MenuUI.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public static bool isPaused;
+    public GameObject pauseMenu;
+
+    public AudioMixer audioMixer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        isPaused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))// Escape Down
+        {
+            if (isPaused) //when game is paused
+            {
+                Resume();
+            }
+            else // when not paused
+            {
+                pauseMenu.SetActive(true);
+                isPaused = true;
+                Time.timeScale = 0; //freezing all the activity
+            }
+        }
+
+        if (Time.timeScale == 0)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+
+    }
+
+    
+    public void Resume()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1; //resume time in game
+    }
+
+    public void VolumeChange(float setVolume)
+    {
+        audioMixer.SetFloat("Volume", setVolume);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void CursorInMenu()
+    {
+       
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
---
+++ NASAProjectSimulation/Assets/Scripts/_player/PlayerScripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+    public int jumpTakeOff;
+    private Rigidbody rb;
+    private bool doubleJump;
+   
+    
+    private float distanceToGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+        jumpTakeOff = 5;
+        speed = 10;
+        doubleJump = false;
+        rb = GetComponent<Rigidbody>();
+        distanceToGround = GetComponent<Collider>().bounds.extents.y;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+            float moveHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            float moveVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+            //Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+            transform.Translate(moveHorizontal, 0, moveVertical);
+            //rb.AddForce(move * speed);
+
+
+            if (Input.GetKeyDown("escape"))
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+
+            if (Input.GetButtonDown("Jump") && IsGrounded())
+            {
+
+                doubleJump = true;
+                rb.AddForce(Vector3.up * jumpTakeOff, ForceMode.Impulse);
+
+            }
+            else if (doubleJump && Input.GetButtonDown("Jump"))
+            {
+                doubleJump = false;
+
+                rb.AddForce(Vector3.up * jumpTakeOff, ForceMode.Impulse);
+
+            }
+            else if(Input.GetButtonDown("Jump"))
+            {
+                Debug.Log("Jumping");
+                rb.AddForce(Vector3.up * jumpTakeOff, ForceMode.Impulse);
+            }
+        
+
+    }
+
+    //Ground Check
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector2.up, distanceToGround + 0.1f);
+
+    }
+}
+
