4cb51108b38472ec2837ba58fae9ec96f2ec11a2
malay10 <malaybhakta10@gmail.com>
2019-11-02 19:01:24 -0500

    Small Models and Talking Avtar Scene

---
+++ NASAProjectSimulation/Assets/AudioManagerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip[] audioClips;
+    private int index = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            if (audioClips[index] && !audioSource.isPlaying)
+                StartCoroutine(AudioInstruction(index++));
+            //index++;
+        }
+    }
+
+    IEnumerator AudioInstruction(int index)
+    {
+        if (audioClips[index])
+        {
+            audioSource.PlayOneShot(audioClips[index], 0.9f);//can scale audio
+            yield return new WaitForSeconds(audioClips[index].length);
+        }
+        else
+        {
+            Debug.Log("No more Clips");
+            yield return new WaitForEndOfFrame();
+
+        }
+    }
+}
---
+++ NASAProjectSimulation/Assets/Scenes/MalayScene's/C# Scrip Non-ARVR/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+                //Getting rax axis for mouse movement
+                Vector2 md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+                //smoothing the camera movemement
+                md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+                smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+                smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+                mouseLook += smoothV;
+
+                transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right); //rotating around right
+                character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+    }
+}
